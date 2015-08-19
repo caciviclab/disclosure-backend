d40850036a24c39ccefa5a459a6c4916d97e3c52
Tom Dooner <tom.dooner@brigade.com>
2015-08-18 20:08:11 -0700

    Attempt to extract calaccess_raw dependency
    I think #15 has the right idea -- the CCDC folks are doing great work,
    and will continue to be doing great work, and forking their
    calaccess_raw module will make it hard to benefit from that work.
    
    However, I am really bad at python, I am now realizing. I removed the
    entire directory and started anew, and ended up with this PR that
    vaguely works. I started the data download, but I have no idea if it
    will finish as of writing.

--- calaccess_raw/admin/base.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-from django.contrib import admin
-
-
-class BaseAdmin(admin.ModelAdmin):
-    save_on_top = True
-
-    def get_readonly_fields(self, *args, **kwargs):
-        return [f.name for f in self.model._meta.fields]
--- cookbook/metadata.rb
+++
@@ -1,2 +0,0 @@
-name 'disclosure-backend'
-version '0.1.0'
