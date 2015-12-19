d162d1810c02119daa16c5755740a4d4dd8fed70
Ben Cipollini <bcipolli@ucsd.edu>
2015-12-18 20:27:47 -0800

    Add missing migration.

---
+++ ballot/migrations/0003_auto_20151219_0359.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ballot', '0002_load_ballot_data'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='contest',
+            options={'verbose_name': 'race'},
+        ),
+        migrations.AlterModelOptions(
+            name='locality',
+            options={'verbose_name_plural': 'localities'},
+        ),
+        migrations.AlterModelOptions(
+            name='referendum',
+            options={'verbose_name': 'ballot measure'},
+        ),
+        migrations.AlterField(
+            model_name='referendum',
+            name='number',
+            field=models.CharField(help_text=b"The referendum's number or letter.", max_length=5),
+        ),
+    ]
