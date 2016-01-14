3ea495d2617e849e1c52de03493a3291e59ceeff
Ben Cipollini <bcipolli@ucsd.edu>
2016-01-14 08:30:20 -0800

    RF: make tests directory, stub parse_ tests.

---
+++ finance/tests/test_command.py
@@ -0,0 +1,31 @@
+import tempfile
+
+from django.core.management import call_command
+from django.test import TestCase, override_settings
+
+from ballot.models import Ballot, BallotItem, BallotItemResponse
+from office_election.models import OfficeElection, Candidate
+from referendum.models import Referendum, ReferendumResponse
+from finance.models import IndependentMoney
+
+
+@override_settings(NETFILE_DOWNLOAD_DIR=tempfile.mkdtemp())
+class XformNetfileRawDataTest(TestCase):
+
+    def test_xformnetfilerawdata(self, test_agency='CSA', test_year='2015'):
+        """
+        Tests a single file download
+        """
+
+        # Smoke test--make sure there are no errors.
+        call_command('xformnetfilerawdata', verbosity=0,
+                     agencies=test_agency, years=test_year)
+
+        # Check data
+        for model in [Ballot, BallotItem, BallotItemResponse,
+                      Referendum, ReferendumResponse,
+                      OfficeElection, Candidate,
+                      IndependentMoney]:
+
+            self.assertTrue(model.objects.all().count() > 0,
+                            '%ss exist after parse' % model.__class__.__name__)
---
+++ finance/tests/test_f460a.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+
+
+class FinanceParseTests(TestCase):
+
+    def test_isnan(self):
+        pass  # return isinstance(val, Number) and np.isnan(val)
+
+    def test_isnone(self):
+        pass  # return val is None or val == 'None'
+
+    def test_parse_benefactor(self):
+        # must use the different entity_Cd
+        # def parse_benefactor(row, verbosity=1):
+        pass
+
+    def test_parse_form_and_report_period(self):
+        pass
+
+    def test_parse_beneficiary(self):
+        pass
+
+    def test_parse_candidate_and_office(self):
+        pass
+
+    def test_parse_candidate_info(self):
+        pass
+
+    def test_parse_referendum_info(self):
+        pass
+
+    def test_parse_ballot_info(self):
+        pass
+
+    def test_get_committee_benefactor(self):
+        pass
+
+    def test_get_committee_beneficiary(self):
+        pass
+
+    def test_clean_filer_id(self):
+        pass
+
+    def test_load_f460A_row(self):  # noqa
+        pass
+
+    def test_load_f460A_data(self):  # noqa
+        pass
