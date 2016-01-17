ea97fba0940ceae62f440869e6e434ba84f43085
Ben Cipollini <bcipolli@ucsd.edu>
2016-01-16 19:05:57 -0800

    TST: add tests for /locations

---
+++ disclosure/tests/test_locations.py
@@ -0,0 +1,29 @@
+from django.db.models import Q
+from rest_framework.test import APITestCase
+
+from finance.tests.test_command import WithForm460ADataTest
+from locality.models import City
+from finance.models import IndependentMoney
+
+
+class LocationTests(WithForm460ADataTest, APITestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        WithForm460ADataTest.setUpClass()
+        APITestCase.setUpClass()
+
+    def test_list_contains_data(self):
+        # Get first city with non-None name
+        city = City.objects.filter(~Q(name=None))[0]
+
+        resp = self.client.get('/locations/%d' % city.id)
+        data = resp.data
+        self.assertEqual(data['contribution_count'],
+                         IndependentMoney.objects.all().count())
+
+        self.assertEqual(data['location']['name'], city.name)
+        self.assertEqual(data['location']['fip_id'], city.id)
+
+        self.assertIn('contribution_by_type', data)
+        self.assertIn('contribution_by_area', data)
