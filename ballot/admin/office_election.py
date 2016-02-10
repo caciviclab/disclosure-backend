002f9119aac30821e74fc315300915c2a21b18ff
Ben Cipollini <bcipolli@ucsd.edu>
2016-02-10 14:02:19 -0500

    BF: hide/show models in admin, to be useful for data entrants.

--- ballot/admin/office_election.py
+++ ballot/admin/office_election.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 
 from ..models import office_election as models
 
-admin.site.register(models.Party)
-admin.site.register(models.Office)
 admin.site.register(models.Candidate)
+admin.site.register(models.OfficeElection)
--- disclosure/admin.py
+++ disclosure/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.db import router
 
 for model in admin.site._registry.keys():
+    # Eliminate *_raw from the admin panel.
     if router.db_for_write(model=model) != 'default':
         admin.site.unregister(model)
