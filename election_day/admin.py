30d4bf5421767c46ae64f29c58059f5189774f54
Ben Cipollini <bcipolli@ucsd.edu>
2016-02-27 07:30:08 -0800

    BF: remove broken ElectionAdmin..

--- election_day/admin.py
+++ election_day/admin.py
@@ -3,6 +3,4 @@ from django.contrib import admin
 from . import models
 
 
-class ElectionDayAdmin(admin.ModelAdmin):
-    list_display = ('Ballot.date', 'Ballot.locality')
-admin.site.register(models.ElectionDay, ElectionDayAdmin)
+admin.site.register(models.ElectionDay)
