4385e3b770d6cbd53228cad312c1a8ae460f46e2
Ben Cipollini <bcipolli@ucsd.edu>
2016-03-05 15:16:04 -0800

    TST: add _django_utils.serializers tests.

---
+++ _django_utils/tests.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.test import TestCase
+
+from _django_utils.serializers import as_money, MagicModelSerializer
+
+
+class AsMoneyTest(TestCase):
+
+    def test_good(self):
+        """
+        Smoke tests on str() and unicode()
+        """
+        self.assertEqual(0, as_money(0))
+        self.assertEqual(1.5, as_money(1.5))
+        self.assertEqual(1.51, as_money(1.51))
+        self.assertTrue(1.496 < as_money(1.496))
+
+    def test_evil(self):
+        self.assertIsNone(as_money(None))
+
+
+class DummyModel(models.Model):
+    f1 = models.CharField()
+    f2 = models.IntegerField()
+
+
+class MagicModelSerializerTest(TestCase):
+    def test_exclude_field(self):
+        class DummySerializer(MagicModelSerializer):
+            class Meta:
+                model = DummyModel
+                exclude_fields = ['f2']
+
+        self.assertIn('f1', DummySerializer().get_fields())
+        self.assertNotIn('f2', DummySerializer().get_fields())
+
+    def test_rename_field(self):
+        class DummySerializer(MagicModelSerializer):
+            class Meta:
+                model = DummyModel
+                rename_fields = dict(f2='foo')
+
+        self.assertIn('foo', DummySerializer().get_fields())
+        self.assertNotIn('f2', DummySerializer().get_fields())
