c65a3611f3d099c7d49d71b5a2058f1a51a42b9b
Aaron D Borden <adborden@a14n.net>
2016-08-23 23:37:02 -0700

    Serialize BallotItems as their subclass

--- ballot/serializers/ballot.py
+++ ballot/serializers/ballot.py
@@ -1,18 +1,59 @@
 from rest_framework import serializers
-from ..models import Ballot, BallotItem
+from ..models import Ballot, BallotItem, Candidate, Office, OfficeElection, Referendum
 from _django_utils import ExtendedModelSerializer
 
 
-class BallotItemSerializer(ExtendedModelSerializer):
+class CandidateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Candidate
+
+
+class OfficeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Office
+
+
+class ReferendumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Referendum
+        exclude = ('ballot',)
+
+
+class OfficeElectionSerializer(serializers.ModelSerializer):
+    candidates = CandidateSerializer(many=True)
+
+    class Meta:
+        model = OfficeElection
+        exclude = ('ballot', 'office')
+
+    def to_representation(self, obj):
+        office = OfficeSerializer(obj.office).data
+        office_election = super(OfficeElectionSerializer, self).to_representation(obj)
+        office.update(office_election)
+        return office
+
+
+class BallotItemSerializer(serializers.ModelSerializer):
     contest_type = serializers.CharField(source='get_contest_type_display')
-    name = serializers.CharField(source='__str__')
+
+    def to_representation(self, obj):
+        ballot_item = super(BallotItemSerializer, self).to_representation(obj)
+        subclass_data = dict()
+        if obj.contest_type == 'R':
+            subclass_data = ReferendumSerializer(obj.referendum).data
+        elif obj.contest_type == 'O':
+            subclass_data = OfficeElectionSerializer(obj.officeelection).data
+
+        subclass_data.update(ballot_item)
+        return subclass_data
 
     class Meta:
         model = BallotItem
+        exclude = ('ballot',)
 
 
 class BallotSerializer(ExtendedModelSerializer):
-    ballot_items = BallotItemSerializer(many=True, read_only=True, exclude=['ballot'])
+    ballot_items = BallotItemSerializer(many=True, read_only=True)
 
     class Meta:
         model = Ballot
